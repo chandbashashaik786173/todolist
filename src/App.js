2f5d635c0bd9a89c1a8a0811b1261961dc3e48d1
shaik chand basha <shaikchandbasha173@gmail.com>
2024-07-31 13:03:31 +0530

    Update client

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+  const fetchTodos = async () => {
+    const response = await axios.get("http://localhost:7865/todos");
+    setTodos(response.data);
+  };
+  const addTodo = async (e) => {
+    e.preventDefault();
+    await axios.post("http://localhost:7865/todos", { text: newTodo });
+    setNewTodo("");
+    fetchTodos();
+  };
+  const deleteTodo = async (id) => {
+    await axios.delete(`http://localhost:7865/todos/${id}`);
+    fetchTodos();
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To-Do App</h1>
+      <form onSubmit={addTodo}>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="Add a new todo"
+        />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo._id}>
+            {todo.text}
+            <button onClick={() => deleteTodo(todo._id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
